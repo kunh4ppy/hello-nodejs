dfb30414b719c884a3026c8c262ed059d8f06a85
dqt <dqt@gmail.com>
2023-09-15 03:18:04 +0000

    change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-app.get('/contact', function (req, res) {
-	res.send('Hello node');
+app.get('/dqt', function (req, res) {
+	res.send('Hello dqt');
 });
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
